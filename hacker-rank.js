207e99ee59ab0eb33601f88ec01db333f47598ba
estein1988 <estein1988@gmail.com>
2020-11-19 13:13:45 -0700

    Add diagonal difference solution.

--- hacker-rank.js
+++ hacker-rank.js
@@ -25,3 +25,15 @@ function compareTriples(a, b){
     }
     return scores
 }
+
+//diagonal difference
+function diagonalDifference(array){
+    let leftToRight = 0
+    let rightToLeft = 0
+    for(let i=0; i < arr.length; i++){
+        leftToRight += array[i][i]
+        rightToLeft += array[i][array.length - i - 1]
+    }
+    let sum = Math.abs(leftToRight - rightToLeft)
+    return sum
+}
